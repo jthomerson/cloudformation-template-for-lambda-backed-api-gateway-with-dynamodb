ce557e97262bb5d8323db4135258a5552cf81445
Jeremy Thomerson <jeremy@thomersonfamily.com>
2016-04-13 00:04:25 -0400

    Very simple real Dynamo POST/GET actually working

--- index.js
+++ index.js
@@ -5,20 +5,58 @@
 
 'use strict';
 
-let doc = require('dynamodb-doc');
-let dynamo = new doc.DynamoDB();
+var AWS = require('aws-sdk'),
+    dynamo = new AWS.DynamoDB.DocumentClient();
 
-/**
- * Provide an event that contains the following keys:
- *
- *   - operation: one of the operations in the switch statement below
- *   - tableName: required for operations that interact with DynamoDB
- *   - payload: a parameter to pass to the operation being performed
- */
 exports.handler = function(event, context, callback) {
-   console.log('Received event:', JSON.stringify(event, null, 2));
+   var method = event.context['http-method'].toUpperCase();
 
-   callback(null, {
-      response: { something: 'test123', evt: event }
-   });
+   switch (method) {
+      case 'GET':
+         handleGet(event, context, callback);
+         break;
+      case 'POST':
+         handlePost(event, context, callback);
+         break;
+      default:
+         callback('Unsupported method');
+   }
 };
+
+function handlePost(event, context, callback) {
+   var params = {
+      TableName: event.custom.DynamoDBTableName,
+      // TODO: obviously there's no request validation going on here like there should be
+      Item: {
+         GUID: event['body-json'].GUID,
+         DueDate: event['body-json'].DueDate,
+         Title: event['body-json'].Title,
+         IsCompleted: event['body-json'].IsCompleted,
+      }
+   };
+
+   dynamo.put(params, function(err, data) {
+      if (err) {
+         callback(null, { customError: 'Error adding item: ' + JSON.stringify(err, null, 2) });
+      } else {
+         callback(null, {
+            data: data
+         });
+      }
+   });
+}
+
+function handleGet(event, context, callback) {
+   var params = {
+      TableName: event.custom.DynamoDBTableName,
+      Limit: 10,
+   };
+
+   dynamo.scan(params, function(err, data) {
+      callback(null, {
+         input: event,
+         err: err,
+         output: data
+      });
+   });
+}
